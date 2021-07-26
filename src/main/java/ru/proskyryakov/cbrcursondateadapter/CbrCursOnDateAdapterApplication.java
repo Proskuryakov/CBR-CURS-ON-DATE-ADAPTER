b3e15272e1c3560f918d05b9138fa58f851e9297
proskuryakov_e_d <proskur.egor@yandex.ru>
2021-07-26 16:12:17 +0300

    Initial commit

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplication.java
@@ -0,0 +1,13 @@
+package ru.proskyryakov.cbrcursondateadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrCursOnDateAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbrCursOnDateAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.proskyryakov.cbrcursondateadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrCursOnDateAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
