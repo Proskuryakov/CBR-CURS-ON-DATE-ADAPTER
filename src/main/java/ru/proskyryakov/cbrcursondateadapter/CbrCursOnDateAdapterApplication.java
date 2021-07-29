9ddcaa3e34de9653b05d1f11f7257a8d6de4c1e9
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-07-29 15:32:19 +0300

    Add lookup test soap request to cbr

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplication.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.proskuryakov.cbrcursondateadapter.cbr.wsdl.GetCursOnDateXMLResponse;
+import ru.proskuryakov.cbrcursondateadapter.cbr.wsdl.ValuteData;
 import ru.proskyryakov.cbrcursondateadapter.cbr.CbrClient;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class CbrCursOnDateAdapterApplication {
@@ -22,10 +24,14 @@ public class CbrCursOnDateAdapterApplication {
         return args -> {
 
             var date = new GregorianCalendar(2021, Calendar.JULY, 27);
-            GetCursOnDateXMLResponse response = client.getCursOnDateXMLResponse(date);
-            System.out.println(response.getGetCursOnDateXMLResult().getContent());
+            var valute = client.getValuteCursOnDate(date);
+            valute.stream().map(this::valuteCursOnDateToString).forEach(System.out::println);
 
         };
+    }
+
+    private String valuteCursOnDateToString (ValuteData.ValuteCursOnDate valuteCursOnDate){
+        return valuteCursOnDate.getVname().trim() + " " + valuteCursOnDate.getVchCode() + " " + valuteCursOnDate.getVcurs();
     }
 
 }
