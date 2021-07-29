3a1e2fb0a8a15cf434b6c3b35e09173ac99d1354
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-07-29 15:17:10 +0300

    Add universal method

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/cbr/CbrClient.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/cbr/CbrClient.java
@@ -1,15 +1,14 @@
 package ru.proskyryakov.cbrcursondateadapter.cbr;
 
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 import ru.proskuryakov.cbrcursondateadapter.cbr.wsdl.GetCursOnDateXML;
 import ru.proskuryakov.cbrcursondateadapter.cbr.wsdl.GetCursOnDateXMLResponse;
 
-import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.GregorianCalendar;
@@ -17,23 +16,19 @@ import java.util.GregorianCalendar;
 public class CbrClient extends WebServiceGatewaySupport {
 
     private static final Logger log = LoggerFactory.getLogger(CbrClient.class);
-    @Value("${cbr.uri}")
-    private String cbrUri;
 
+    @SneakyThrows
     public GetCursOnDateXMLResponse getCursOnDateXMLResponse(GregorianCalendar date){
         GetCursOnDateXML request  = new GetCursOnDateXML();
+        XMLGregorianCalendar calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(date);
+        request.setOnDate(calendar);
 
-        try {
-            XMLGregorianCalendar calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(date);
-            log.info(calendar.toXMLFormat());
-            request.setOnDate(calendar);
-        } catch (DatatypeConfigurationException e) {
-            e.printStackTrace();
-        }
+        return (GetCursOnDateXMLResponse) getResponse(request, "http://web.cbr.ru/GetCursOnDateXML");
+    }
 
-        return (GetCursOnDateXMLResponse) getWebServiceTemplate()
-                .marshalSendAndReceive(request, new SoapActionCallback("http://web.cbr.ru/GetCursOnDateXML"));
-
+    public Object getResponse(Object request, String soapAction) {
+        log.info("Execute request " + soapAction);
+        return getWebServiceTemplate().marshalSendAndReceive(request, new SoapActionCallback(soapAction));
     }
 
 }
