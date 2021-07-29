f3d24a2fef3edcb954ae2d957d7db8a5f9d94ed8
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-07-29 16:55:42 +0300

    Create controller and add method get curs by code

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/api/CursController.java
@@ -0,0 +1,24 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.proskyryakov.cbrcursondateadapter.adapter.models.CursOnDate;
+import ru.proskyryakov.cbrcursondateadapter.adapter.services.CursService;
+
+@RestController
+@RequestMapping("/curs")
+@RequiredArgsConstructor
+public class CursController {
+    private final CursService cursService;
+
+    @GetMapping("/{code}")
+    public CursOnDate getCursByCode(@PathVariable String code){
+        return cursService.getCursByCode(code);
+    }
+
+
+
+}
