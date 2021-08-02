b63bfb0a866004f8f1d4ffe3a2364625bedbd6b7
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-02 13:27:13 +0300

    Create bean for CacheStorage

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplication.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/CbrCursOnDateAdapterApplication.java
@@ -2,12 +2,20 @@ package ru.proskyryakov.cbrcursondateadapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.proskyryakov.cbrcursondateadapter.cache.CacheStorage;
 
 @SpringBootApplication
 public class CbrCursOnDateAdapterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CbrCursOnDateAdapterApplication.class, args);
+    }
+
+    @Bean
+    public <K, V> CacheStorage<K, V> cacheStorage() {
+        CacheStorage<K, V> cacheStorage = new CacheStorage<>();
+        return cacheStorage;
     }
 
 }
