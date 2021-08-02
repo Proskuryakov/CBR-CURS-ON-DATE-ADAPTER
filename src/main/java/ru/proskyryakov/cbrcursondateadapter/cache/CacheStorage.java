2e33829186855b5bad3200b0f675060a64cfe0f4
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-02 14:03:49 +0300

    Change map to concurrent map and add save time

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/cache/CacheStorage.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/cache/CacheStorage.java
@@ -1,22 +1,48 @@
 package ru.proskyryakov.cbrcursondateadapter.cache;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class CacheStorage<K, V> {
 
-    private final Map<K, V> cache;
+    public static final Long DEFAULT_SAVE_TIME = 86_400L;
+
+    private final ConcurrentMap<K, DateValue<V>> cache;
+    private final Long saveTime;
 
     public CacheStorage() {
-        cache = new HashMap<>();
+        this(DEFAULT_SAVE_TIME);
+    }
+
+    public CacheStorage(Long saveTime) {
+        cache = new ConcurrentHashMap<>();
+        this.saveTime = saveTime;
     }
 
     public void add(K key, V value) {
-        cache.put(key, value);
+        cache.put(key, new DateValue<>(value, saveTime));
     }
 
     public V get(K key) {
-        return cache.get(key);
+        var dateValue = cache.get(key);
+        if(dateValue == null) return null;
+        return dateValue.value;
+    }
+
+    private static class DateValue<V> {
+        public V value;
+        public Date date;
+
+        public DateValue(V value, Long saveTime) {
+            this.value = value;
+            date = new Date();
+            date.setTime(date.getTime() + saveTime);
+        }
+
+        public boolean isLive() {
+            return date.getTime() > new Date().getTime();
+        }
     }
 
 }
