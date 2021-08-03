2b9c4ca07cf24107ec12439c1f2b97720d13d617
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-03 15:06:33 +0300

    Use NotFoundException

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/api/CursController.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/api/CursController.java
@@ -2,6 +2,7 @@ package ru.proskyryakov.cbrcursondateadapter.adapter.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.proskyryakov.cbrcursondateadapter.adapter.exceptions.NotFoundException;
 import ru.proskyryakov.cbrcursondateadapter.adapter.models.CodeWithDates;
 import ru.proskyryakov.cbrcursondateadapter.adapter.models.CursOnDate;
 import ru.proskyryakov.cbrcursondateadapter.adapter.services.CursService;
@@ -15,13 +16,17 @@ public class CursController {
     private final CursService cursService;
 
     @GetMapping("/{code}")
-    public CursOnDate getCursByCode(@PathVariable String code){
-        return cursService.getCursByCode(code);
+    public CursOnDate getCursByCode(@PathVariable String code) throws NotFoundException {
+        var result = cursService.getCursByCode(code);
+        if(result == null) throw new NotFoundException(String.format("Curs by %s code not found", code));
+        return result;
     }
 
     @GetMapping("/{code}/date/{date}")
-    public CursOnDate getCursByCodeAndDate(@PathVariable String code, @PathVariable String date){
-        return cursService.getCursByCodeAndDate(code, date);
+    public CursOnDate getCursByCodeAndDate(@PathVariable String code, @PathVariable String date) throws NotFoundException {
+        var result = cursService.getCursByCodeAndDate(code, date);
+        if(result == null) throw new NotFoundException(String.format("Curs by %s code not found", code));
+        return result;
     }
 
     @PostMapping
