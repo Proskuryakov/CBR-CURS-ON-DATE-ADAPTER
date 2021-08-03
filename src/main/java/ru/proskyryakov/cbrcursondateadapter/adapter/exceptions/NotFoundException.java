0d343fd388403a0ba8be013fd1b9754aca6176bf
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-03 15:04:58 +0300

    Create exception for empty result

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.exceptions;
+
+public class NotFoundException extends Exception{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
