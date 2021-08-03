b6af92c0bd54ed99addf36328a78914bb529e836
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-03 15:06:04 +0300

    Create exception for date convert

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/exceptions/DateConversionException.java
@@ -0,0 +1,9 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.exceptions;
+
+public class DateConversionException extends Exception{
+
+    public DateConversionException(String message) {
+        super(message);
+    }
+
+}
