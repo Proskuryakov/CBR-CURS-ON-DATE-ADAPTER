a4999f3218d8c6a020aadc44fcc3cf88f0cdfa65
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-03 15:04:31 +0300

    Create model for exception response

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/exceptions/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private String message;
+
+}
