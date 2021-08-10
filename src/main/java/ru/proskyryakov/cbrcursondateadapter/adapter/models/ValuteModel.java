aed7358282b2997bad14dafe16738c2d4a53b7ef
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-10 14:03:13 +0300

    Create valute model for controller

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/models/ValuteModel.java
@@ -0,0 +1,11 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.models;
+
+import lombok.Data;
+
+@Data
+public class ValuteModel {
+
+    public String code;
+    public Integer interval;
+
+}
