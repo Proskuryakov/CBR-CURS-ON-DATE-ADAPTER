40e43f62053a8e5a4f6848c8e9bd891c1e6fee50
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-10 17:49:24 +0300

    Add post and put operations

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/mappers/ValuteMapper.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/mappers/ValuteMapper.java
@@ -1,7 +1,7 @@
 package ru.proskyryakov.cbrcursondateadapter.adapter.mappers;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapping;
 import ru.proskyryakov.cbrcursondateadapter.adapter.models.ValuteModel;
 import ru.proskyryakov.cbrcursondateadapter.db.entities.Valute;
 
@@ -10,10 +10,14 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface ValuteMapper {
 
+    @Mapping(target = "code", source = "valute.code")
+    @Mapping(target = "interval", source = "valute.interval")
     ValuteModel toValuteModel(Valute valute);
 
     List<ValuteModel> toValuteModels(List<Valute> valutes);
 
+    @Mapping(target="code", source="valuteModel.code")
+    @Mapping(target="interval", source="valuteModel.interval")
     Valute fromValuteModel(ValuteModel valuteModel);
 
     List<Valute> fromValuteModels(List<ValuteModel> valuteModels);
