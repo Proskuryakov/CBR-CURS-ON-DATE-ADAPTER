4ed49fed2d630ffd4570336f153c363d39c5aea1
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-11 14:10:52 +0300

    Add exception and handler for already exists db error

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/exceptions/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.exceptions;
+
+public class AlreadyExistsException extends Exception{
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
