acc16ee142fac62a709e88c30f6506de3a55ac2f
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-11 14:08:14 +0300

    Add new db entity

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/db/entities/Interval.java
@@ -0,0 +1,20 @@
+package ru.proskyryakov.cbrcursondateadapter.db.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "intervals")
+@Data
+public class Interval {
+
+    @Id
+    @GeneratedValue(generator = "intervals_id_seq")
+    private Long id;
+    private Integer interval;
+
+    @OneToOne
+    @JoinColumn(name = "valute_id", referencedColumnName = "id")
+    private Valute valute;
+
+}
