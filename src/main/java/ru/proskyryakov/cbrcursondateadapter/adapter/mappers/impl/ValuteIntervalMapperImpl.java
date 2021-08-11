0c349637542f99808779ca506059c13e55f783cf
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-11 14:09:06 +0300

    Change mapper for new models

---
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/mappers/impl/ValuteIntervalMapperImpl.java
@@ -0,0 +1,52 @@
+package ru.proskyryakov.cbrcursondateadapter.adapter.mappers.impl;
+
+import org.springframework.stereotype.Component;
+import ru.proskyryakov.cbrcursondateadapter.adapter.mappers.ValuteIntervalMapper;
+import ru.proskyryakov.cbrcursondateadapter.adapter.models.IntervalModel;
+import ru.proskyryakov.cbrcursondateadapter.adapter.models.ValuteModel;
+import ru.proskyryakov.cbrcursondateadapter.db.entities.Interval;
+import ru.proskyryakov.cbrcursondateadapter.db.entities.Valute;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ValuteIntervalMapperImpl implements ValuteIntervalMapper {
+    @Override
+    public ValuteModel toValuteModel(Valute valute) {
+        ValuteModel valuteModel = new ValuteModel();
+        valuteModel.setCode(valute.getCode());
+        valuteModel.setId(valute.getId());
+        valuteModel.setName(valute.getName());
+        return valuteModel;
+    }
+
+    @Override
+    public List<ValuteModel> toValuteModels(List<Valute> valutes) {
+        return valutes.stream().map(this::toValuteModel).collect(Collectors.toList());
+    }
+
+    @Override
+    public IntervalModel toIntervalModel(Interval interval) {
+        return new IntervalModel(
+                interval.getId(),
+                interval.getValute().getId(),
+                interval.getValute().getCode(),
+                interval.getInterval()
+        );
+    }
+
+    @Override
+    public List<IntervalModel> toIntervalModels(List<Interval> intervals) {
+        return intervals.stream().map(this::toIntervalModel).collect(Collectors.toList());
+    }
+
+    @Override
+    public Interval fromIntervalModel(IntervalModel intervalModel, Valute valute) {
+        var interval = new Interval();
+        interval.setInterval(intervalModel.getInterval());
+        interval.setValute(valute);
+        interval.setId(null);
+        return interval;
+    }
+}
