7b6f16126ba261f164fc9237e812deb5bfe716b8
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-16 14:37:05 +0300

    Add generated value

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/db/entities/History.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/db/entities/History.java
@@ -1,13 +1,19 @@
 package ru.proskyryakov.cbrcursondateadapter.db.entities;
 
+import lombok.Data;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
+@Data
+@ToString
 public class History {
 
     @Id
+    @GeneratedValue(generator = "history_id_seq")
     private Long id;
     @ManyToOne
     @JoinColumn(name = "interval_id", referencedColumnName = "id")
