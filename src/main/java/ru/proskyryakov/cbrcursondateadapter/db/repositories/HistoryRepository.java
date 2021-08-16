38256a5b1a4c10a61dba8575dc94aab318617a06
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-16 14:39:00 +0300

    Add native query for find all latest history entry

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/db/repositories/HistoryRepository.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/db/repositories/HistoryRepository.java
@@ -1,6 +1,7 @@
 package ru.proskyryakov.cbrcursondateadapter.db.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.proskyryakov.cbrcursondateadapter.db.entities.History;
 
@@ -8,5 +9,14 @@ import java.util.List;
 
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long> {
+
+    @Query(
+            nativeQuery = true,
+            value = "select h.* from history h, " +
+                    "(select interval_id, max(datetime) as datetime from history " +
+                    "    join intervals i on i.id = interval_id where i.is_actual group by interval_id) as nh " +
+                    "where h.interval_id = nh.interval_id and h.datetime = nh.datetime"
+    )
+    List<History> findAllLastEntry();
 
 }
