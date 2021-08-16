e3a277e312987b8abf95da2aa36c95c8b79338eb
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-16 14:43:47 +0300

    Add scheduled update of history valute curs change

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/scheduler/SchedulerService.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/scheduler/SchedulerService.java
@@ -1,21 +1,82 @@
 package ru.proskyryakov.cbrcursondateadapter.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import ru.proskyryakov.cbrcursondateadapter.adapter.models.CursOnDate;
 import ru.proskyryakov.cbrcursondateadapter.adapter.services.CursHistoryService;
+import ru.proskyryakov.cbrcursondateadapter.adapter.services.CursService;
+import ru.proskyryakov.cbrcursondateadapter.db.entities.History;
+import ru.proskyryakov.cbrcursondateadapter.db.entities.Interval;
+import ru.proskyryakov.cbrcursondateadapter.db.entities.Valute;
+import ru.proskyryakov.cbrcursondateadapter.db.repositories.HistoryRepository;
+import ru.proskyryakov.cbrcursondateadapter.db.repositories.IntervalRepository;
 
-import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
 
+    private static final Logger log = LoggerFactory.getLogger(SchedulerService.class);
+
     private final CursHistoryService cursHistoryService;
+    private final CursService cursService;
+    private final IntervalRepository intervalRepository;
+    private final HistoryRepository historyRepository;
+
+    @Value("${scheduler.interval-coefficient}")
+    private long intervalCoeff;
 
     @Scheduled(fixedDelayString = "${scheduler.fixedDelay}", initialDelayString = "${scheduler.initialDelay}")
     public void start() {
-        System.out.println("Работаю с задержкой " + LocalDateTime.now());
+        log.info("Scheduled work");
+        List<History> histories = historyRepository.findAllLastEntry();
+
+        Date date = new Date();
+        List<Valute> tracedValutes = getTrackedValute(histories, date);
+
+        tracedValutes.addAll(
+                intervalRepository.findNewIntervals().stream()
+                        .map(Interval::getValute)
+                        .collect(Collectors.toList())
+        );
+
+        if (!tracedValutes.isEmpty()) {
+           addToHistory(tracedValutes, date);
+        }
+    }
+
+    private List<Valute> getTrackedValute(List<History> histories, Date currentDate) {
+        return histories.stream().filter(h ->
+                currentDate.after(new Date(h.getDatetime().getTime() + h.getInterval().getInterval() * intervalCoeff)))
+                .map(h -> h.getInterval().getValute())
+                .collect(Collectors.toList());
+    }
+
+    private void addToHistory(List<Valute> tracedValutes, Date currentDate) {
+        List<CursOnDate> cursOnDateList = cursService.getCursByCodesAndDate(
+                tracedValutes.stream().map(Valute::getCode).collect(Collectors.toList()),
+                currentDate
+        );
+
+        Map<String, Interval> intervals = intervalRepository.findAllByIsActualTrue().stream()
+                .collect(Collectors.toMap(i -> i.getValute().getCode(), i -> i));
+
+        for(CursOnDate curs: cursOnDateList) {
+            History h = new History();
+            h.setCurse(curs.getCurs());
+            h.setDatetime(currentDate);
+            h.setInterval(intervals.get(curs.getCode()));
+            historyRepository.save(h);
+        }
+
     }
 
 }
