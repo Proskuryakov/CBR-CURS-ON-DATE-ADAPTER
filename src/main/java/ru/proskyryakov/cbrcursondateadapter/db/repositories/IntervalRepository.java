7b060cb3b6f0fd15825d355ebc63c40615e17bb3
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-16 14:40:01 +0300

    Add method for find interval without logging into history

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/db/repositories/IntervalRepository.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/db/repositories/IntervalRepository.java
@@ -1,6 +1,7 @@
 package ru.proskyryakov.cbrcursondateadapter.db.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.proskyryakov.cbrcursondateadapter.db.entities.Interval;
 
 import java.util.List;
@@ -12,4 +13,12 @@ public interface IntervalRepository extends JpaRepository<Interval, Long> {
     Interval findIntervalByIsActualTrueAndValute_Code(String code);
 
     List<Interval> findAllByIsActualTrue();
+
+    @Query(
+            nativeQuery = true,
+            value = "select i.* from intervals i left " +
+                    "join history h on i.id = h.interval_id where h.id is null and i.is_actual"
+    )
+    List<Interval> findNewIntervals();
+
 }
