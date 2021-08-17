a7dd50913b0a6a41b4c3e83c1d98384d439bf735
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-17 13:45:58 +0300

    Fill history table

--- src/main/resources/db/migration/V0_0_1_20210811_1550__fill_history_test_data.sql
+++ src/main/resources/db/migration/V0_0_1_20210811_1550__fill_history_test_data.sql
@@ -1,2 +1,2 @@
-insert into history(interval_id, curse)
-values (1, 70), (1, 80);
+insert into history(interval_id, curse, datetime)
+values (1, 70, '2021-08-11 11:08:54+03'), (1, 80, '2021-08-12 11:08:54+03');
