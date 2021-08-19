3e8007142dc325089f3e0ebf76aa50112e2cd8e9
Egor Proskuryakov <proskur.egor@yandex.ru>
2021-08-19 17:56:51 +0300

    Add constructors

--- src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/models/ValuteModel.java
+++ src/main/java/ru/proskyryakov/cbrcursondateadapter/adapter/models/ValuteModel.java
@@ -1,8 +1,12 @@
 package ru.proskyryakov.cbrcursondateadapter.adapter.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValuteModel {
 
     private Long id;
